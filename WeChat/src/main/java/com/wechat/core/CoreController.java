f1b7e27a488f41a84f4466801aac2bd1d36f51d4
lllxxxsss <632410877@qq.com>
2017-06-09 14:54:37 +0800

    hah

--- WeChat/src/main/java/com/wechat/core/CoreController.java
+++ WeChat/src/main/java/com/wechat/core/CoreController.java
@@ -3,7 +3,11 @@ package com.wechat.core;
 import javax.servlet.http.HttpServletRequest;
 
 public interface CoreController {
-
+/**
+ * 
+ * @param request
+ * @return
+ */
 	
 	public String execute(HttpServletRequest request);
 }
